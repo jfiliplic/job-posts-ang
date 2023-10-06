ee19ea96db2679275f128b158cc991bed3d18404
jfiliplic <janezfiliplic@gmail.com>
2023-10-06 14:54:07 +0200

    created+defined data service, used it in home cmp

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { DataService, JobPost } from '../../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  title = 'home';
+  data: JobPost[] = [];
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((response) => {
+      this.data = response;
+      console.log(this.data);
+    });
+  }
 }
